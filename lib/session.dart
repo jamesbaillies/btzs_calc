86ba1da12c8e825c8133f59318d4ba7568059db0
jamesbaillies <jamesbaillies@gmail.com>
2025-04-10 20:27:40 -0600

    lates update 3

--- lib/session.dart
+++ lib/session.dart
@@ -1,53 +1,54 @@
-
 import 'package:flutter/material.dart';
 
 class Session extends ChangeNotifier {
   // Camera Page
   String exposureTitle = '';
   String filmHolder = '';
-  String filmStock = '';
-  List<String> filmStocks = ['HP5', 'Tri-X', 'FP4'];
-  double focalLength = 150;
+  List<String> filmStocks = ['HP5+', 'Tri-X', 'FP4', 'Delta 100'];
+  String filmStock = 'HP5+';
+  double focalLength = 150.0;
   List<int> savedFocalLengths = [90, 150, 210, 300];
 
   // Metering Page
+  int meteringMode = 0; // 0 = Incident, 1 = Zone
+  int incidentEV = 10;
   int lowEV = 5;
   int lowZone = 2;
-  int highEV = 12;
+  int highEV = 15;
   int highZone = 8;
-  int incidentEV = 10;
-  String meteringMode = 'Lo'; // or 'Hi'
 
   // DOF Page
   String dofMode = 'None'; // None, Check, Distance, Focus
-  bool favorDOF = true; // true = favor DOF, false = favor exposure
-  int aperture = 8;
-  List<int> apertureValues = [4, 5, 5, 8, 11, 16, 22, 32, 45];
-  double subjectDistance = 1;
-  double nearDistance = 1;
-  double farDistance = 1;
-  double focusTravel = 0;
+  bool favorDOF = true;
+  double focusNear = 1000.0;
+  double focusFar = 2000.0;
+  double nearDistance = 1.0; // in meters
+  double farDistance = 2.0; // in meters
+  double focusTravel = 5.0; // in mm
+  double subjectDistance = 1.0;
+  double circleOfConfusion = 0.03;
 
-  // Exposure Page
-  bool _useApertureMode = true;
-  int selectedApertureIndex = 4;
-  int selectedMin = 0;
-  int selectedSec = 1;
-  int selectedFraction = 0;
-
-  List<String> shutterFractions = ['1', '2', '4', '8', '15', '30', '60', '125', '250', '500'];
-
-  bool get useApertureMode => _useApertureMode;
-  set useApertureMode(bool value) {
-    _useApertureMode = value;
-    notifyListeners();
-  }
-
+  // Aperture
+  List<int> apertureValues = [4, 5, 5, 6, 7, 8, 9, 11, 13, 14, 16, 18, 20, 22, 25, 28, 32, 36, 40, 45];
+  int _aperture = 8;
   int get aperture => _aperture;
   set aperture(int value) {
     _aperture = value;
     notifyListeners();
   }
 
-  int _aperture = 8;
+  // Exposure Page
+  bool _useApertureMode = true;
+  bool get useApertureMode => _useApertureMode;
+  set useApertureMode(bool value) {
+    _useApertureMode = value;
+    notifyListeners();
+  }
+
+  int selectedApertureIndex = 5; // default: f/8
+  int selectedMin = 0;
+  int selectedSec = 1;
+  int selectedFraction = 0;
+
+  List<int> shutterFractions = [1, 2, 4, 8, 15, 30, 60, 125, 250, 500, 1000];
 }
